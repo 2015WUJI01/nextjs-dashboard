cf5e2f3497ed3003d048546f8226aebb36e763ee
Eli Wu <eliwu@soarinfotech.com>
2024-09-30 00:23:25 +0800

    feat: Chapter3 Optimizeing Fonts and Images

---
+++ app/ui/fonts.ts
@@ -0,0 +1,12 @@
+import { LXGW_WenKai_TC, Lusitana } from 'next/font/google';
+
+export const inter = LXGW_WenKai_TC({
+    subsets: ["latin"],
+    weight: "700",
+});
+
+
+export const lusitana = Lusitana({
+    subsets: ["latin"],
+    weight: "400",
+})
